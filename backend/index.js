1350b84dc2edfa65fde977d200b64df19506cf5c
THOMAS0034 <107304775+THOMAS0034@users.noreply.github.com>
2024-07-12 19:19:52 +0530

    frontend

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,18 @@ import express, { response } from "express";
 import { PORT ,URL } from "./config.js";
 import mongoose from "mongoose";
 import {book} from "./models/bookmodel.js";
+import cors from 'cors'
+
 
 const app=express();
+
+app.use(
+    cors({
+        origin:'http://localhost:3000',
+        methods:['GET','POST','PUT','DELETE'],
+        allowedHeaders:['Content-Type'],
+    })
+)
 
 app.use(express.json())
 
@@ -12,51 +22,13 @@ app.get('/',(req,res)=>{
 });
 
 //Route for new book
-app.post('/books', async (req,res)=>{
-    try{
-        if(!req.body.title ||
-            !req.body.author||
-            !req.body.publishyear
-        ){
-            return res.status(400).send({
-                message:"Required field are not filled",
-            })
-        } 
-        const newbook={
-            title:req.body.title,
-            author:req.body.author,
-            publishyear:req.body.publishyear
-        }
-        const Book=await book.create(newbook)
-        return res.status(201).send(Book)
-    }
-    catch(error){
-        console.log(error)
-    }
-})
-
-app.get('/books/:id', async (req,res) => {
-    try {
-        const {id} =req.params;
-
-
-        const Books =await book.findById(id) 
-        return res.status(200).json(Books);
-        
-    }catch (error) {
-        console.log(error.message)
-        res.status(500).send({message:error.message})
-    }
-})
-
-
 
 mongoose
 .connect(URL)
 .then(()=>{
     console.log("connected")
     app.listen(PORT,()=>{
-        console.log("hello")
+        console.log(`Server running at port ${PORT}`)
     })
 })
 .catch((error)=>{
